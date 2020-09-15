443a4139d5105c93588c15ef38d230779de4c7cd
Conor Longmire <55465929+C3Longmire11@users.noreply.github.com>
2020-09-15 16:27:35 -0500

    Add files via upload

---
+++ Customer.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SportsProBusinessClassLibrary
+{
+    public class Customer
+    {
+        private int customerID;
+        private string name;
+        private string address;
+        private string city;
+        private string state;
+        private string zipCode;
+        private string phone;
+        private string email;
+
+        public int CustomerID
+        {
+            get { return customerID; }
+            set { customerID = value; }
+        }
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+        public string Address
+        {
+            get { return address; }
+            set { address = value; }
+        }
+        public string City
+        {
+            get { return city; }
+            set { city = value; }
+        }
+        public string State
+        {
+            get { return state; }
+            set { state = value; }
+        }
+
+        public string ZipCode
+        {
+            get { return zipCode; }
+            set { zipCode = value; }
+        }
+
+        public string Phone
+        {
+            get { return phone; }
+            set { phone = value; }
+        }
+        public string EMail
+        {
+            get { return email; }
+            set { email = value; }
+        }
+
+        public Customer() { }
+    }
+}
