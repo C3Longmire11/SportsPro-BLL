d762aa3b28a8f9f2e4d478c35ffc522229f44b94
Conor Longmire <55465929+C3Longmire11@users.noreply.github.com>
2020-09-15 16:28:24 -0500

    Add files via upload

---
+++ RegisteredIncident.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SportsProBusinessClassLibrary
+{
+    public class RegisteredIncident
+    {
+        private int customerID;
+        private string customerName;
+        private DateTime? dateOpened;
+        private DateTime? dateClosed;
+        private string productName;
+        private decimal version;
+        private string techName;
+        private string techEmail;
+        private string techPhone;
+        private string state;
+
+
+
+        public int CustomerID
+        {
+            get { return customerID; }
+            set { customerID = value; }
+        }
+
+
+
+        public string CustomerName
+        {
+            get { return customerName; }
+            set { customerName = value; }
+        }
+
+
+        public DateTime? DateOpened
+        {
+            get { return dateOpened; }
+            set { dateOpened = value; }
+        }
+
+
+        public DateTime? DateClosed
+        {
+            get
+            { return dateClosed;}
+            set { dateClosed = value; }
+        }
+
+        public string ProductName
+        {
+            get { return productName; }
+            set { productName = value; }
+        }
+
+
+        public decimal Version
+        {
+            get { return version; }
+            set { version = value; }
+        }
+
+
+        public string TechName
+        {
+            get { return techName; }
+            set { techName = value; }
+        }
+
+
+        public string TechEmail
+        {
+            get { return techEmail; }
+            set { techEmail = value; }
+        }
+
+
+        public string TechPhone
+        {
+            get { return techPhone; }
+            set { techPhone = value; }
+        }
+
+
+
+        public string State
+        {
+            get { return state; }
+            set { state = value; }
+        }
+
+    }
+}
